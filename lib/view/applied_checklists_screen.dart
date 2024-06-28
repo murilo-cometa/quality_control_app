7621ce43fed5c46dc4daa3ab0f23af98818cd9ed
murilo-cometa <murilocometasupermercado@gmail.com>
2024-06-28 17:25:08 -0300

    meu último commit :(

--- lib/view/applied_checklists_screen.dart
+++ lib/view/applied_checklists_screen.dart
@@ -1,33 +1,14 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:quality_control_app/common/component/custom_appbar.dart';
 
 class AppliedChecklistsScreen extends StatelessWidget {
   AppliedChecklistsScreen({super.key});
 
-  final _random = Random();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar.build('Checklists aplicados'),
-      body: ListView.builder(
-        padding: const EdgeInsets.all(10),
-        shrinkWrap: true,
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          int storeCode = _random.nextInt(40) + 1;
-          int checklistType = _random.nextInt(10) + 1;
-          String title = '$checklistType na loja $storeCode';
-          // return SimpleNavigatingCardItem(
-          //   text: title,
-          //   goTo: Checklist(
-          //     editMode: false,
-          //     title: title,
-          //   ),
-          // );
-        },
-      ),
+      body: const Placeholder()
     );
   }
 }
--- lib/view/apply_checklist_screen.dart
+++ lib/view/apply_checklist_screen.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:numberpicker/numberpicker.dart';
 import 'package:quality_control_app/common/component/custom_appbar.dart';
+import 'package:quality_control_app/common/library/custom_navigator.dart';
+import 'package:quality_control_app/view/checklist_with_db.dart';
 
 class ApplyChecklistScreen extends StatefulWidget {
   const ApplyChecklistScreen({super.key});
@@ -10,11 +13,16 @@ class ApplyChecklistScreen extends StatefulWidget {
 }
 
 class _ApplyChecklistScreenState extends State<ApplyChecklistScreen> {
-
   int _selectedStore = 1;
+
+  // travou a aplicação :(
 
   @override
   Widget build(BuildContext context) {
+    final CollectionReference myDB = FirebaseFirestore.instance
+        .collection('stores')
+        .doc(_selectedStore.toString())
+        .collection('checklists');
     return Scaffold(
       appBar: CustomAppBar.build('Aplicar checklist'),
       body: Column(
@@ -39,14 +47,44 @@ class _ApplyChecklistScreenState extends State<ApplyChecklistScreen> {
             value: _selectedStore,
             onChanged: (value) {
               setState(() {
-              _selectedStore = value;
+                _selectedStore = value;
               });
             },
           ),
           const Divider(),
-          // Expanded(
-          //   child: ListView.builder(),
-          // ),
+          StreamBuilder(
+            stream: myDB.snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    DocumentSnapshot doc = snapshot.data!.docs[index];
+                    String title = doc['title'];
+                    String id = doc.id;
+                    return Card(
+                      elevation: 3,
+                      child: ListTile(
+                        title: Text(title),
+                        onTap: () {
+                          CustomNavigator.goTo(
+                            context: context,
+                            destination: ChecklistWithDb(
+                                editMode: false,
+                                checklistName: title,
+                                collection: myDB.doc(id).collection('tasks')),
+                          );
+                        },
+                      ),
+                    );
+                  },
+                );
+              }
+              return const Center(
+                child: Text('não há checklists'),
+              );
+            },
+          )
         ],
       ),
     );
